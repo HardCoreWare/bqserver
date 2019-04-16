21d3467befdaf300ac396793bc50e45adf410b15
HardCoreWare <bernardocodu@gmail.com>
2019-04-15 22:21:42 -0500

    listo

--- app/core/manager.php
+++ app/core/manager.php
@@ -4,6 +4,6 @@ require_once '../app/vendor/autoload.php';
 require_once '../app/lib/BigQuery.php';
 require_once '../app/model/interface/BigQueryInterface.php';
 require_once '../app/model/primitive/BigQueryConnection.php';
-require_once '../app/model/module/BigQueryConnection.php';
+require_once '../app/model/module/Demo.php';
 
 ?>
