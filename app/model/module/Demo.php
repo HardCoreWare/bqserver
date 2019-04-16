afda1229cc916766d981c92c883da3fbd28ad76e
HardCoreWare <bernardocodu@gmail.com>
2019-04-15 20:58:57 -0500

    OK

--- app/model/module/Demo.php
+++ app/model/module/Demo.php
@@ -21,7 +21,14 @@ class Demo extends Bseg
     
     public function ammount($params){
 
-
+        " SELECT HKONT AS account, BUDAT AS ".
+        " FROM ".
+        " (SELECT BUDAT,DMBTR,HKONT FROM `informe-211921.MULTIVA.BSEGAIO`".
+        " WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = 1".
+        " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = 2019 ".
+        " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'OPERADORA')".
+        " AND HKONT = '6410010103'); ";
+        
 
     }
 
