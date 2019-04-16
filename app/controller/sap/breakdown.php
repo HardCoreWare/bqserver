9a6dce25b345afda64519a9564002f1f8e168da7
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 13:25:52 -0500

    agregando centro de costos

--- app/controller/sap/breakdown.php
+++ app/controller/sap/breakdown.php
@@ -2,26 +2,26 @@
 
 class breakdown extends Controller{
 
-    public function day($year,$month,$day,$account,$enterprises){
+    public function day($year,$month,$day,$account,$enterprise){
 
         $demo = new BreakdownModel(new BigQuery('informe-211921'));
-        $breakdown = $demo->day(["year"=>$year,"month"=>$month,"day"=>$day,"account"=>$account,"enterprise"=>$enterprises]);
+        $breakdown = $demo->day(["year"=>$year,"month"=>$month,"day"=>$day,"account"=>$account,"enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
         
     }
  
-    public function month($year,$month,$account,$enterprises){
+    public function month($year,$month,$account,$enterprise){
 
         $demo = new BreakdownModel(new BigQuery('informe-211921'));
-        $breakdown = $demo->month(["year"=>$year,"month"=>$month,"account"=>$account,"enterprise"=>$enterprises]);
+        $breakdown = $demo->month(["year"=>$year,"month"=>$month,"account"=>$account,"enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
         
     }
 
-    public function year($year,$account,$enterprises){
+    public function year($year,$account,$enterprise){
 
         $demo = new BreakdownModel(new BigQuery('informe-211921'));
-        $breakdown = $demo->year(["year"=>$year,"account"=>$account,"enterprise"=>$enterprises]);
+        $breakdown = $demo->year(["year"=>$year,"account"=>$account,"enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
         
     }
