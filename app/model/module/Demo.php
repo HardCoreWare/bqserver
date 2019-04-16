2d3ac21bac0e00cd90fdbdea5523dd831d75481f
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 10:14:54 -0500

    ok

--- app/model/module/Demo.php
+++ app/model/module/Demo.php
@@ -13,7 +13,7 @@ class Demo extends Connection{
         $sql=
         " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, ".$params['enterprise']." AS enterprise ".
         " FROM ".
-        " (SELECT BUDAT,DMBTR FROM `informe-211921.BALANZA.BSEGAIO`".
+        " (SELECT BUDAT,DMBTR FROM `informe-211921.MULTIVA.BSEGAIO`".
         " WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = 1".
         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = 2019".
         " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
