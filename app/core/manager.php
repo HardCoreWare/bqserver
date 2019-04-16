d3fd5a01e89d3c35fad100dace900786d097e609
HardCoreWare <bernardocodu@gmail.com>
2019-04-15 19:38:49 -0500

    okay

--- app/core/manager.php
+++ app/core/manager.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once '../app/vendor/autoload.php';
 require_once '../app/lib/BigQuery.php';
 require_once '../app/model/interface/BigQueryInterface.php';
 require_once '../app/model/primitive/BigQueryPrimitive.php';
