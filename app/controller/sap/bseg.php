5ba80696db563dcc37521d4b8117993a96b9fdee
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 10:04:54 -0500

    ok

--- app/controller/sap/bseg.php
+++ app/controller/sap/bseg.php
@@ -5,10 +5,15 @@ class bseg extends Controller{
     public function breakdown($year,$month,$account,$enterprise){
 
         echo($year."-".$month."-".$account."-".$enterprise);
-
         $demo = new Demo(new BigQuery('informe-211921'));
         $breakdown = $demo->breakdown(["year"=>$year,"month"=>$month,"account"=>$account,"enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
+        
+    }
+
+    public function subtotals($year,$month,$account,$enterprise){
+
+
         
     }
 
--- app/model/module/Demo.php
+++ app/model/module/Demo.php
@@ -4,7 +4,16 @@ class Demo extends Connection{
 
     public function breakdown($params){
 
-        
+        $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO
+        FROM 
+        (SELECT BUDAT,DMBTR FROM `informe-211921.BALANZA.BSEG_2019_1`
+        WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = 1
+        AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = 2019
+        AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')
+        AND DMBTR = 'N0212019001'
+        AND HKONT = '6410010102');";
+
+        $breakdown = $this->bigQueryLib->select($sql);
 
     }
     
