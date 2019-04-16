77d48691bc559153116143c3be86805e010df23b
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 10:08:56 -0500

    ok

--- app/core/manager.php
+++ app/core/manager.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../app/lib/interface/BigQuery.php';
+require_once '../app/lib/BigQuery.php';
 require_once '../app/model/interface/ConnectionInterface.php';
 require_once '../app/model/primitive/Connection.php';
 require_once '../app/model/module/Demo.php';
