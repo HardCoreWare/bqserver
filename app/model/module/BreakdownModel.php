446b0776dc73db3c9c85ac362204cc0879bf0f54
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 12:54:11 -0500

    ok

--- app/model/module/BreakdownModel.php
+++ app/model/module/BreakdownModel.php
@@ -20,7 +20,8 @@ class BreakdownModel extends Connection{
         " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$params['enterprise']."' AS enterprise, '".$params['account']."' AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate".
         " FROM ".
         " (SELECT BUKRS, BUDAT, DMBTR, SGTXT FROM `informe-211921.MULTIVA.BSEGAIO`".
-        " WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$params['month'].
+        " WHERE ".
+        " CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$params['month'].
         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$params['year'].
         " AND CAST(SUBSTR(BUDAT,7,2) AS INT64) = ".$params['day'].
         " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
