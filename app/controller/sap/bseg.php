1732c322a4c05c0c989bb1d849013e5481340b7a
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 09:40:52 -0500

    ok

--- app/controller/sap/bseg.php
+++ app/controller/sap/bseg.php
@@ -4,6 +4,8 @@ class bseg extends Controller{
  
     public function breakdown($year,$month,$account,$enterprise){
 
+        echo($year."-".$month."-"."-".$account);
+
         //$demo = new Demo(new BigQuery('informe-211921'));
         //$breakdown = $demo->breakdown(["year"=>$year,"month"=>$month,"account"=>$account,"enterprise"=>$enterprise]);
         //echo(json_encode($breakdown));
