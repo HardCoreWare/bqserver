cbe9662a7a4f216c0a57df1a1283585efbeac2e9
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 13:03:23 -0500

    agregando centro de costos

--- app/model/module/BreakdownModel.php
+++ app/model/module/BreakdownModel.php
@@ -41,7 +41,7 @@ class BreakdownModel extends Connection{
         $sql=
         " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$params['enterprise']."' AS enterprise, '".$params['account']."' AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate".
         " FROM ".
-        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT FROM `informe-211921.MULTIVA.BSEGAIO`".
+        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL FROM `informe-211921.MULTIVA.BSEGAIO`".
         " WHERE".
         " CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$params['month'].
         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$params['year'].
@@ -62,7 +62,7 @@ class BreakdownModel extends Connection{
         $sql=
         " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$params['enterprise']."' AS enterprise, '".$params['account']."' AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate".
         " FROM ".
-        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT FROM `informe-211921.MULTIVA.BSEGAIO`".
+        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL FROM `informe-211921.MULTIVA.BSEGAIO`".
         " WHERE".
         " CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$params['year'].
         " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
