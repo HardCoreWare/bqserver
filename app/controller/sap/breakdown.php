443f95936dcb68dd0dab3b24e1c903977bdea66f
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 14:00:00 -0500

    agregando centro de costos

--- app/controller/sap/breakdown.php
+++ app/controller/sap/breakdown.php
@@ -4,6 +4,8 @@ class breakdown extends Controller{
 
     public function day($year,$month,$day,$account,$enterprise){
 
+        header();
+
         $demo = new BreakdownModel(new BigQuery('informe-211921'));
         $breakdown = $demo->day(["year"=>$year,"month"=>$month,"day"=>$day,"account"=>$account,"enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
