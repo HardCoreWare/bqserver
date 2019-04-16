f953bf930260b1e1ee4e05b60894904f11dd63cc
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 10:17:27 -0500

    ok

--- app/model/module/Demo.php
+++ app/model/module/Demo.php
@@ -19,6 +19,8 @@ class Demo extends Connection{
         " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
         " AND HKONT = '6410010102');";
 
+        echo($sql);
+
         $breakdown = $this->bigQueryLib->select($sql);
 
         return $breakdown;
