499cb35a2b00dbee6c776f7cf076281004d9d388
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 16:06:24 -0500

    agregando modelo de tablas

---
+++ app/controller/sap/breakdowntable.php
@@ -0,0 +1,43 @@
+<?php
+
+class breakdowntable extends Controller{
+
+    public function day($year,$month,$day,$account,$enterprise){
+
+        header("Content-Type: application/json");
+        $demo = new BreakdownTableModel(new BigQuery('informe-211921'));
+        $breakdown = $demo->day(["year"=>$year,"month"=>$month,"day"=>$day,"account"=>$account,"enterprise"=>$enterprise]);
+        echo(json_encode($breakdown));
+        
+    }
+ 
+    public function month($year,$month,$account,$enterprise){
+
+        header("Content-Type: application/json");
+        $demo = new BreakdownTableModel(new BigQuery('informe-211921'));
+        $breakdown = $demo->month(["year"=>$year,"month"=>$month,"account"=>$account,"enterprise"=>$enterprise]);
+        echo(json_encode($breakdown));
+        
+    }
+
+    public function year($year,$account,$enterprise){
+
+        header("Content-Type: application/json");
+        $demo = new BreakdownTableModel(new BigQuery('informe-211921'));
+        $breakdown = $demo->year(["year"=>$year,"account"=>$account,"enterprise"=>$enterprise]);
+        echo(json_encode($breakdown));
+        
+    }
+
+    public function all($account,$enterprise){
+
+        header("Content-Type: application/json");
+        $demo = new BreakdownTableModel(new BigQuery('informe-211921'));
+        $breakdown = $demo->all(["account"=>$account,"enterprise"=>$enterprise]);
+        echo(json_encode($breakdown));
+        
+    }
+
+}
+
+?>
--- app/core/manager.php
+++ app/core/manager.php
@@ -5,3 +5,4 @@ require_once '../app/lib/BigQuery.php';
 require_once '../app/model/interface/ConnectionInterface.php';
 require_once '../app/model/primitive/Connection.php';
 require_once '../app/model/module/BreakdownModel.php';
+require_once '../app/model/module/BreakdownTableModel.php';
