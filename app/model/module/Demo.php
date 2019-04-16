1dc16cf906196a120c3f88e52ac8d44af31b61aa
HardCoreWare <bernardocodu@gmail.com>
2019-04-16 10:14:08 -0500

    ok

--- app/model/module/Demo.php
+++ app/model/module/Demo.php
@@ -10,13 +10,14 @@ class Demo extends Connection{
 
     public function breakdown($params){
 
-        $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO
-        FROM 
-        (SELECT BUDAT,DMBTR FROM `informe-211921.BALANZA.BSEG_2019_1`
-        WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = 1
-        AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = 2019
-        AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')
-        AND HKONT = '6410010102');";
+        $sql=
+        " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, ".$params['enterprise']." AS enterprise ".
+        " FROM ".
+        " (SELECT BUDAT,DMBTR FROM `informe-211921.BALANZA.BSEGAIO`".
+        " WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = 1".
+        " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = 2019".
+        " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
+        " AND HKONT = '6410010102');";
 
         $breakdown = $this->bigQueryLib->select($sql);
 
