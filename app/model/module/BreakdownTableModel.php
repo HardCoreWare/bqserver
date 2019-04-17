c9d8634eab28e9a8ca8585aa3523e38db8679fdd
HardCoreWare <bernardocodu@gmail.com>
2019-04-17 04:39:00 -0500

    listo

--- app/model/module/BreakdownTableModel.php
+++ app/model/module/BreakdownTableModel.php
@@ -55,36 +55,54 @@ class BreakDownTableModel extends Connection{
 
     public function year($params){
 
+        $enterpriseArray=explode("-",$params['enterprise']);
 
-        $sql=
-        " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$params['enterprise']."' AS enterprise, HKONT AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate, KOSTL AS costCenter".
-        " FROM ".
-        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL, HKONT FROM `informe-211921.MULTIVA.BSEGAIO`".
-        " WHERE".
-        " CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$params['year'].
-        " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
-        " AND HKONT IN (SELECT DISTINCT(account) FROM `informe-211921.MULTIVA.CUENTAS`))".
-        " ORDER BY CAST(BUDAT AS INT64);";
-        //echo($sql.'<br><br><br>');
+        $subquerys=[];
 
-        $breakdown = $this->bigQueryLib->select($sql);
+        for ($i=0; $i <count($enterprises) ; $i++) { 
 
-        return $breakdown;
+            $enterprise=$enterprises[$i];
+
+            $sql=
+            " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$enterprise."' AS enterprise, HKONT AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate, KOSTL AS costCenter".
+            " FROM ".
+            " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL, HKONT FROM `informe-211921.MULTIVA.BSEGAIO`".
+            " WHERE".
+            " CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$params['year'].
+            " AND KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = '".$enterprise."')".
+            " AND HKONT IN (SELECT DISTINCT(account) FROM `informe-211921.MULTIVA.CUENTAS`))";
+            //echo($sql.'<br><br><br>');
+
+            $subquerys[]=$sql;
+
+        }
+
+        $uniquery=implode(" UNION ALL ",$subquerys)." ORDER BY CAST(BUDAT AS INT64);";
+
+        $breakdown = $this->bigQueryLib->select($uniquery);
+
 
     }
 
     public function all($params){
 
-        $enterprises=explode("-",$params['enterprise']);
+        $enterpriseArray=explode("-",$params['enterprise']);
 
-        $sql=
-        " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$params['enterprise']."' AS enterprise, HKONT AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate, KOSTL AS costCenter".
-        " FROM ".
-        " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL, HKONT FROM `informe-211921.MULTIVA.BSEGAIO`".
-        " WHERE".
-        " KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = 'BANCO')".
-        " AND HKONT IN (SELECT DISTINCT(account) FROM `informe-211921.MULTIVA.CUENTAS`))".
-        " ORDER BY CAST(BUDAT AS INT64);";
+        for ($i=0; $i <count($enterprises) ; $i++) { 
+
+            $enterprise=$enterprises[$i];
+
+            $sql=
+            " SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS ammount, '".$enterprise."' AS enterprise, HKONT AS account,  BUKRS as sapSociety, CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS documentDate, KOSTL AS costCenter".
+            " FROM ".
+            " (SELECT BUKRS, BUDAT, DMBTR, SGTXT, KOSTL, HKONT FROM `informe-211921.MULTIVA.BSEGAIO`".
+            " WHERE".
+            " KOSTL IN (SELECT KOSTL FROM `informe-211921.MULTIVA.CECOS` WHERE MODULO = '".$enterprise."')".
+            " AND HKONT IN (SELECT DISTINCT(account) FROM `informe-211921.MULTIVA.CUENTAS`))".
+            " ORDER BY CAST(BUDAT AS INT64);";
+
+        }
+
         //echo($sql.'<br><br><br>');
 
         $breakdown = $this->bigQueryLib->select($sql);
