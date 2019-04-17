7f7d7aaedbfd0a40f04aba615f97262fc95c18cf
HardCoreWare <bernardocodu@gmail.com>
2019-04-17 12:36:22 -0500

    revisando clase de conexion

--- app/controller/sap/breakdowntable.php
+++ app/controller/sap/breakdowntable.php
@@ -34,7 +34,7 @@ class breakdowntable extends Controller{
     public function all($enterprise){
 
         header("Content-Type: application/json");
-        $demo = new BreakdownTableModel(new BigQuery('informe-211921'));
+        $demo = new BreakdownTableModel(BigQuery::getInstance('informe-211921'));
         $breakdown = $demo->all(["enterprise"=>$enterprise]);
         echo(json_encode($breakdown));
         
